2a5130a05dea8a8050b23de72763bf64e8a52948
Oktavianus Chua <ata.chai10@gmail.com>
2021-08-04 21:28:31 +0700

    admin: to register color and range

--- colors/admin.py
+++ colors/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models        import Color, Range
 
-# Register your models here.
+
+class RangeInLine(admin.TabularInline):
+    model = Range
+    extra = 0
+
+
+class ColorAdmin(admin.ModelAdmin):
+    inlines = (RangeInLine,)
+
+
+admin.site.register(Color, ColorAdmin)
