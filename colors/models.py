58af26d5aff127550c048be28fb0277ddc7feb82
Oktavianus Chua <ata.chai10@gmail.com>
2021-08-04 21:28:23 +0700

    models, migrations: add new model for color and ranges

--- colors/models.py
+++ colors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+    
+
+class Range(models.Model):
+    name = models.CharField(max_length=20)
+    range_start = models.IntegerField()
+    range_stop = models.IntegerField()
+    suffix = models.CharField(max_length=10, blank=True)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name='ranges')
