1c13e8104ebfda1710de682b7bb9fdf4cde5998f
Oktavianus Chua <ata.chai10@gmail.com>
2021-08-04 21:31:19 +0700

    settings, views, urls: to add color api

--- colors/urls.py
+++ colors/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-
+from .views import ColorAPIView
 
 app_name = 'colors'
 urlpatterns = [
+    path('colors/', ColorAPIView.as_view(), name='index')
 ]
--- colors/views.py
+++ colors/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import random
+from django.conf                import settings
+from rest_framework             import status
+from rest_framework.response    import Response
+from rest_framework.views       import APIView
+from .models                    import Color
 
-# Create your views here.
+
+class ColorAPIView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        color_ids = list(Color.objects.all().values_list('id', flat=True))
+        result = []
+        if not color_ids:
+            return Response(result, status=status.HTTP_200_OK)
+        for i in range(settings.DEFAULT_SWATCHES_SET):
+            color = Color.objects.get(id=random.choice(color_ids))
+            res = {'type': color.name}
+            for r in color.ranges.all():
+                res[r.name] = '%(value)s%(suffix)s' % {'value': random.randint(r.range_start, r.range_stop), 'suffix': r.suffix}
+            result.append(res)
+        return Response(result, status=status.HTTP_200_OK)
